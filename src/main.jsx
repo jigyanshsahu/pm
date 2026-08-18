cb851c106e535a0e5a9aeeab87cf42f402594b46
Jigyansh sahu <indianinja008@gmail.com>
2026-08-18 06:17:50 +0530

    test init

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { store } from './app/store.js'
 import { Provider } from 'react-redux'
-
+import { ClerkProvider } from '@clerk/react'
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
+     <ClerkProvider>
             <Provider store={store}>
                 <App />
             </Provider>
+            </ClerkProvider>
     </BrowserRouter>,
 )
