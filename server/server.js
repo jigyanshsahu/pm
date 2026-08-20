b620785e8a2cd292e8a7fbd5a7035983655cd0d3
Jigyansh sahu <indianinja008@gmail.com>
2026-08-20 06:45:51 +0530

    feat: integrate Inngest for user management and add Vercel configurations
    - Added Inngest as a dependency to handle user events.
    - Implemented functions to sync user creation, deletion, and updates with Prisma.
    - Updated server.js to include Inngest middleware.
    - Created vercel.json for client and server to manage routing and deployment.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ import 'dotenv/config';
 import cors from 'cors';
 import { clerkMiddleware } from '@clerk/express';
 
+import {serve}  from "inngest/express";
+import {inngest,functions} from "./inngest/index.js"
+import { serveEndpoint } from 'inngest/node';
+
 const app = express();
 
 app.use((express.json));
@@ -13,5 +17,10 @@ app.use(clerkMiddleware());
 app.get('/',(req,res)=>{
     res.send('server is live');
 })
+app.use('/api/inngest', serve({client:inngest,functions}));
+serveEndpoint
+
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=> console.log(`the server in running on ${PORT}`));
